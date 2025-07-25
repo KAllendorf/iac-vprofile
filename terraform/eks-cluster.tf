3da6575762b87fa5877fdf38fcbf434277c51263
KAllendorf <kyleallendorf@gmail.com>
2025-07-25 14:02:32 -0500

    Testing new EKS settings

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,6 +5,12 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
+  addons = {
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+  }
+
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
