1e09ea0d629a433b6482995a154683a58968b73c
KAllendorf <kyleallendorf@gmail.com>
2025-07-25 14:04:57 -0500

    Testing Workflow Fixes

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,15 +5,15 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
-  addons = {
-    eks-pod-identity-agent = {
-      before_compute = true
-    }
-  }
-
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    eks_pod_identity_agent = {
+      most_recent = true
+    }
+  }
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
